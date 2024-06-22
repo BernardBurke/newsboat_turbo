0a06a55cfb786b5568e9c999683a1eb7e9cd2def
Ben Burke <ben@bernard.francis.burke@gmail.com>
2024-06-23 06:31:52 +1000

    useable yt audio

--- nbt_ytdlp-audio-interface.sh
+++ nbt_ytdlp-audio-interface.sh
@@ -1,8 +1,49 @@
-if [[ $1 == "" ]]; then
+if [[ "$1" == "" ]]; then
 	echo "Please provide a URL"
 	exit 1
 fi
 
 PODPATH=~/Poddle
 
-yt-dlp -f 139 $1 -o "$PODPATH/%(uploader)s/%(title)s.%(ext)s"
+# write the current time and $1 into >> $HOME/yix_data.log
+echo "$(date) $1" >> $HOME/yix_data.log
+
+get_uploader() {
+
+	youtube_url="$1"
+
+	# Get video info in JSON format
+	video_info=$(yt-dlp -j "$youtube_url")
+
+	# Extract and print uploader
+	uploader=$(echo "$video_info" | jq -r '.uploader')
+
+	if [[ -z "$uploader" ]]; then
+		echo ""	
+	else
+		echo "$uploader"
+	fi
+}
+
+
+# Get uploader
+uploader=$(get_uploader "$1")
+
+if [[ $uploader == "" ]]; then
+	echo "Uploader not found in json - using the default uploader string"
+	PODOUTUPTSTRING="$PODPATH/%(uploader)s/%(title)s.%(ext)s"
+else
+	# replace any spaces in $uploader with underscores
+	uploader=$(echo "$uploader" | sed 's/ /_/g')
+	PODOUTUPTSTRING="$PODPATH/$uploader/%(title)s.%(ext)s"
+fi
+
+
+yt-dlp -f 139 "$1" -o "$PODOUTUPTSTRING"
+
+
+# log the $1 and $PODOUTUPTSTRING to a file in $HOME called yix_data.log
+echo "$1" "$PODOUTUPTSTRING" >> $HOME/yix_data.log
+
+
+
---
+++ nbt_ytdlp_all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# read all the urls in $1 and call $NBT/nbt_ytdlp-audio-interface.sh for each one
+# $1 is a file with one URL per line
+
+if [[ "$1" == "" ]]; then
+    echo "Please provide a file with URLs"
+    exit 1
+fi
+
+while IFS= read -r url; do
+    $NBT/nbt_ytdlp-audio-interface.sh "$url"
+done < "$1"
+
+# ask the user if they want to delete the file with the URLs
+read -p "Delete the file with URLs? [y/N] " -n 1 -r
