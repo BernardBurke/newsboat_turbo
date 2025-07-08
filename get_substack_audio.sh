9a15f92007f7c25e202468c24ae104b59ae297d0
Ben Burke <ben@bernard.francis.burke@gmail.com>
2025-07-09 05:49:43 +1000

    get audio url from substack

---
+++ get_substack_audio.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# if $1 is not set, exit
+if [ -z "$1" ]; then
+  echo "Usage: $0 <path_to_venv>"
+  exit 1
+fi  
+
+file_name=$(basename "$1" .html)
+
+# if $2 is a directory, use it as the output directory
+if [ -d "$2" ]; then
+  output_dir="$2"
+else
+  output_dir="."
+fi
+
+url=$(grep -oP 'audio_url\\":\\"\K(https?://.*?)\\"' https___kyla.substack.com_p_from-dollar-dominance-to-the-slop.html | sed 's/\\"$//' | head -n 1)
+
+echo "Downloading audio from $url"
+
+# use curl to download the audio file tot he output directory/file_name.mp3
+curl -L "$url" -o "$output_dir/$file_name.mp3"  
+if [ $? -eq 0 ]; then
+  echo "Audio downloaded successfully to $output_dir/$file_name.mp3"
+else
+  echo "Failed to download audio."
+  exit 1
+fi
