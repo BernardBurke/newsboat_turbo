4d1e44b5e48c1cd631bcb60ee4f2a11d9dfe8a7a
Ben Burke <ben@bernard.francis.burke@gmail.com>
2025-07-09 06:22:17 +1000

    need to revisit to get JSON

--- get_substack_audio.sh
+++ get_substack_audio.sh
@@ -5,7 +5,35 @@ if [ -z "$1" ]; then
   exit 1
 fi  
 
-file_name=$(basename "$1" .html)
+# if $1 is a valid file, do this
+if [  -f "$1" ]; then
+  file_name=$(basename "$1" .html)
+  tempfile="$1"
+#else
+# todo, parse the JSON for a title
+# if [ -z "$tempfile" ]; then 
+#   tempfile=$(mktemp)
+#   curl -L "$1" -o "$tempfile"
+
+#   if [ $? -ne 0 ]; then
+#     echo "Failed to download the HTML file."
+#     exit 1
+#   fi
+
+# fi  
+  # AUDIO_JSON=$(yt-dlp -j "$1")  
+
+  # title=$(echo "$AUDIO_JSON" | jq -r '.title')
+  # title=$(echo "$title" | sed 's/[^[:alnum:] ]//g')
+  # title=$(echo "$title" | sed 's/^[ \t]*//;s/[ \t]*$//')
+  # echo "Title: $title"
+  # file_name=$(echo "$title" | tr ' ' '_')  # replace spaces with underscores
+  # # add .mp3 extension to the file name
+  # file_name="${file_name}.mp3"
+  
+fi
+
+
 
 # if $2 is a directory, use it as the output directory
 if [ -d "$2" ]; then
@@ -14,7 +42,7 @@ else
   output_dir="."
 fi
 
-url=$(grep -oP 'audio_url\\":\\"\K(https?://.*?)\\"' https___kyla.substack.com_p_from-dollar-dominance-to-the-slop.html | sed 's/\\"$//' | head -n 1)
+url=$(grep -oP 'audio_url\\":\\"\K(https?://.*?)\\"' $tempfile | sed 's/\\"$//' | head -n 1)
 
 echo "Downloading audio from $url"
 
